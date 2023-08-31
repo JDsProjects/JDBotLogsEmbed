f1f1b0836190729c828e4a4e53074c97cd42ee34
JDJG <ghostzap123@gmail.com>
2023-08-30 22:33:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot()
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("e$"), intents=discord.Intents.all(), strip_after_prefix=True)
 
 #so far this.
