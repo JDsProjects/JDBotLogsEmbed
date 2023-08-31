203d45526b7717751138c1c7a7e17d27c7c753ac
JDJG <ghostzap123@gmail.com>
2023-08-30 22:34:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("e$"), intents=discord.Intents.all(), strip_after_prefix=True)
 
+#will be subclassed soon.
+
 #so far this.
