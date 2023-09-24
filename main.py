6a1efac05c5188e72ba3644c0ecd3d665b3f5cee
JDJG <ghostzap123@gmail.com>
2023-09-24 12:56:01 -0400

    First changes.

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+import traceback
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("e$"), intents=discord.Intents.all(), strip_after_prefix=True)
 
-#will be subclassed soon.
+from cogs import EXTENSIONS
+
+
+class EmbedBot(commands.Bot):
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+    async def setup_hook(self) -> None:
+        for cog in EXTENSIONS:
+            try:
+                await self.load_extension(f"{cog}")
+            except commands.errors.ExtensionError:
+                traceback.print_exc()
+
+
+
+bot = EmbedBot(command_prefix=commands.when_mentioned_or("e$"), intents=discord.Intents.all(), strip_after_prefix=True)
+
 
 #so far this.
+
+bot.run("TOKEN")
