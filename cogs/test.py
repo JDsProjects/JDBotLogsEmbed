caa5bfb8cbc36fd1fa719196681126285cf8fab6
JDJG <ghostzap123@gmail.com>
2023-09-25 19:11:38 -0400

    Test this

--- cogs/test.py
+++ cogs/test.py
@@ -44,6 +44,34 @@ class Embed(commands.Cog):
 
         else:
             await interaction.response.send_message("User detected, ignoring this.")
+
+
+    @app_commands.command(description="leave embed test", name="leave")
+    async def leave(self, interaction: discord.Interaction, member : typing.Union[discord.Member, discord.User]):
+
+        
+        member = member or interaction.user
+
+        if isinstance(member, discord.Member):
+
+            try:
+                pos = sorted(member.guild.members, key=lambda m: m.joined_at or m.created_at).index(member) + 1
+
+            except:
+                pos = "N/A"
+
+            embed = discord.Embed(description=f"This person was the {humanize.ordinal(pos)} out of {len(member.guild.members)} members to join.",timestamp=member.joined_at,color=random.randint(0, 16777215))
+
+            embed.set_footer(text=f"ID: {member.id}")
+
+            embed.set_author(name=f"{member} just left {member.guild.name}", icon_url=member.display_avatar.url)
+
+            embed.set_thumbnail(url=member.guild.icon.url if member.guild.icon else "https://i.imgur.com/3ZUrjUP.png")
+
+            await interaction.response.send_message("There", embed=embed)
+
+        else:
+            await interaction.response.send_message("User detected, ignoring this.")
     
 async def setup(bot):
     await bot.add_cog(Embed(bot))
