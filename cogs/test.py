9c736f19371ce94ca7b4d2db12269e5b12449c18
JDJG <ghostzap123@gmail.com>
2023-09-25 18:11:36 -0400

    ok

--- cogs/test.py
+++ cogs/test.py
@@ -24,7 +24,7 @@ class Embed(commands.Cog):
 
         if isinstance(member, discord.Member):
 
-            embed = discord.Embed(title=f"{member} just joined {member.guild.name}", timestamp=member.joined_at(),color=random.randint(0, 16777215))
+            embed = discord.Embed(title=f"{member} just joined {member.guild.name}", timestamp=member.joined_at,color=random.randint(0, 16777215))
 
             embed.set_footer(text=f"User ID: {member.id}")
 
